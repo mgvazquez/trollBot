ec308f1049e4eb0a6a0496ca4531d4f4dfc43b9e
Manuel Andres Garcia Vazquez <mvazquez@scabb-island.com.ar>
2018-10-01 16:19:53 -0300

    Added CSS

--- edgewise.go
+++ edgewise.go
@@ -8,10 +8,10 @@ import (
 func edgewise(rtm *slack.RTM, msg *slack.MessageEvent) {
 
 	var (
-		rta string = ""
-		img string = ""
-		params = slack.NewPostMessageParameters()
-		match = false
+		rta    string = ""
+		img    string = ""
+		params        = slack.NewPostMessageParameters()
+		match         = false
 	)
 
 	BotName := &rtm.GetInfo().User.Name
@@ -58,7 +58,7 @@ func edgewise(rtm *slack.RTM, msg *slack.MessageEvent) {
 
 	case stringMatch(msg.Text, "magia", "magic"):
 		rta = "<@" + msg.User + ">, " + "deja de tirar tanta magia!"
-		params.Attachments = append(params.Attachments, slack.Attachment{ImageURL:"https://media.tenor.co/images/5fb8e3d4c56cdf53fb15356f8fd4987e/raw"})
+		params.Attachments = append(params.Attachments, slack.Attachment{ImageURL: "https://media.tenor.co/images/5fb8e3d4c56cdf53fb15356f8fd4987e/raw"})
 		match = true
 
 	case stringMatch(msg.Text, "fernet", "fernandito", "fernando"):
@@ -83,7 +83,7 @@ func edgewise(rtm *slack.RTM, msg *slack.MessageEvent) {
 
 	case stringMatch(msg.Text, "proxy"):
 		rta = randomResponse(
-			"Nah Nah Nah"+" <@"+msg.User+">, " + "otro proxy más?!?!",
+			"Nah Nah Nah"+" <@"+msg.User+">, "+"otro proxy más?!?!",
 			":kuaker:, alguien dijo PROXY?!?! :trollface:")
 		match = true
 
@@ -92,13 +92,20 @@ func edgewise(rtm *slack.RTM, msg *slack.MessageEvent) {
 			"https://scontent-eze1-1.xx.fbcdn.net/v/t1.0-9/17103571_258039787985510_4543998202758018301_n.jpg?_nc_cat=0&oh=8f6bcf31d076e8025c7651cae16c5ea0&oe=5BEED0DA",
 			"https://scontent-eze1-1.xx.fbcdn.net/v/t1.0-9/16711881_249105438878945_4294265477870217883_n.jpg?_nc_cat=0&oh=84eff3784bebffeff4c1e4dc03d93127&oe=5C34C56B",
 			"https://scontent-eze1-1.xx.fbcdn.net/v/t1.0-9/17191448_259605207828968_1304022088973812803_n.jpg?_nc_cat=0&oh=f722acd747ecd7ff2eb6f1cb67c00161&oe=5C39F704")
-		params.Attachments = append(params.Attachments, slack.Attachment{ImageURL:img})
+		params.Attachments = append(params.Attachments, slack.Attachment{ImageURL: img})
+		match = true
+
+	case stringMatch(msg.Text, "css", "style-sheet"):
+		img = randomResponse(
+			"https://i.imgur.com/Q3cUg29.gif",
+			"https://i.imgur.com/kv7Eods.png")
+		params.Attachments = append(params.Attachments, slack.Attachment{ImageURL: img})
 		match = true
 
 	case stringMatch(msg.Text, ".net"):
 		img = randomResponse(
 			"https://scontent-eze1-1.xx.fbcdn.net/v/t1.0-9/22310395_361813147608173_1565090941484148499_n.jpg?_nc_cat=0&oh=c395effab9a8eae2c295f778ce29cbb6&oe=5BF043F4")
-		params.Attachments = append(params.Attachments, slack.Attachment{ImageURL:img})
+		params.Attachments = append(params.Attachments, slack.Attachment{ImageURL: img})
 		match = true
 
 	case stringMatch(msg.Text, "node", "angular", "javascript", "js"):
@@ -110,7 +117,7 @@ func edgewise(rtm *slack.RTM, msg *slack.MessageEvent) {
 			"https://i.imgur.com/5LizKB8.png",
 			"https://scontent-eze1-1.xx.fbcdn.net/v/t1.0-9/15541620_218053788650777_1713605560900849432_n.jpg?_nc_cat=0&oh=466e0a28fc468b33aa1688b6a871f207&oe=5C21EA83",
 			"http://gsferreira.com/images/reduce-the-path-length-of-your-node-js-project-dependencies-dependencies-everywhere.jpg")
-		params.Attachments = append(params.Attachments, slack.Attachment{ImageURL:img})
+		params.Attachments = append(params.Attachments, slack.Attachment{ImageURL: img})
 		match = true
 
 	case stringMatch(msg.Text, "chrome", "browser"):
@@ -119,7 +126,7 @@ func edgewise(rtm *slack.RTM, msg *slack.MessageEvent) {
 			"https://scontent-eze1-1.xx.fbcdn.net/v/t31.0-8/23799936_378361682619986_7479532257492459262_o.jpg?_nc_cat=0&oh=d6f028930e946e2cde667d148cc5b3e7&oe=5C2FA87B",
 			"https://scontent-eze1-1.xx.fbcdn.net/v/t1.0-9/22528485_365306187258869_2768680746694188101_n.png?_nc_cat=0&oh=d979f339594c2c0793a454fa5e27dff3&oe=5C321A3B",
 			"https://scontent-eze1-1.xx.fbcdn.net/v/t1.0-9/23130696_370944810028340_6625228377914658080_n.png?_nc_cat=0&oh=3f7f0d6e6d9f2bb2d1325f91c4f4f6e4&oe=5C1FD66E")
-		params.Attachments = append(params.Attachments, slack.Attachment{ImageURL:img})
+		params.Attachments = append(params.Attachments, slack.Attachment{ImageURL: img})
 		match = true
 
 	case stringMatch(msg.Text, "php"):
@@ -128,12 +135,12 @@ func edgewise(rtm *slack.RTM, msg *slack.MessageEvent) {
 			"https://scontent-eze1-1.xx.fbcdn.net/v/t1.0-9/18740765_304542476668574_872412301619614653_n.png?_nc_cat=0&oh=5b8eb9a9826662429339a57bc9e94bd6&oe=5C27AA03",
 			"https://scontent-eze1-1.xx.fbcdn.net/v/t1.0-9/21766449_356108781511943_2077038482413584533_n.png?_nc_cat=0&oh=4b736a2d81a33775a8f9135b2ee3c10c&oe=5C21CB6B",
 			"https://scontent-eze1-1.xx.fbcdn.net/v/t1.0-9/30124133_434089967047157_870403579823885059_n.jpg?_nc_cat=0&oh=71c825feae6d72cdb8c4f6bf7a02d6fe&oe=5C313279")
-		params.Attachments = append(params.Attachments, slack.Attachment{ImageURL:img})
+		params.Attachments = append(params.Attachments, slack.Attachment{ImageURL: img})
 		match = true
 
 	case stringMatch(msg.Text, "ojo", "ojito", "willy"):
 		rta = "*Big Brother is Watching You.*"
-		params.Attachments = append(params.Attachments, slack.Attachment{ImageURL:"https://i.imgur.com/5KASKiY.png"})
+		params.Attachments = append(params.Attachments, slack.Attachment{ImageURL: "https://i.imgur.com/5KASKiY.png"})
 		match = true
 
 	}
